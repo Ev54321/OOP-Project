b1189eba4deef17ab1884810bdc9d3c07e39feb4
Hirayama_Evan <hirae675@hsd.k12.or.us>
2025-06-02 09:45:26 -0700

    Fixing errors

--- src/main/java/com/resturaunt/PrimaryController.java
+++ src/main/java/com/resturaunt/PrimaryController.java
@@ -1,24 +1,24 @@
 package com.resturaunt;
 
+import java.io.IOException;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 
 public class PrimaryController {
+    ImageView logoImage = new ImageView();
+    Button addSodaButton = new Button();
+    Button addWaterButton = new Button();
+    Button addBurgerButton = new Button();
+    Button addSaladButton = new Button();
+    Button addSandwichButton = new Button();
+    Button addSpaghettiButton = new Button();
+    Button payButton = new Button();
+    TextField seedText = new TextField();
 
     @FXML
-    private Button GetFoodButton1;
-
-    @FXML
-    private Button GetFoodButton2;
-
-    @FXML
-    private Button GetFoodButton3;
-
-    @FXML
-    private Button GetFoodButton4;
-
-    @FXML
-    private ImageView Logo;
-
+    private void switchToSecondary() throws IOException {
+        App.setRoot("secondary");
+    }
 }
